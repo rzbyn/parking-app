321cdea6c080823deb394e355ac4ca680392ac48
abe <abe@rzbyn.id>
2024-02-07 08:42:47 +0700

    feat(main): Add logging and ConfigService
    Use environment variable and expose URL for easy access

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get<number>('PORT', 3000);
+
+  await app.listen(PORT);
+
+  logger.log(`Application is listening on port ${PORT}`);
+  logger.log(`http://localhost:${PORT}`);
 }
 bootstrap();
