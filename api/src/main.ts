c417e5c38147120dac9169bc6409e4187d6f2a01
abe <abe@rzbyn.id>
2024-02-07 10:06:15 +0700

    feat(main): Configure Swagger
    Configure swagger for API documentations

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger(bootstrap.name);
@@ -9,6 +10,14 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const PORT = configService.get<number>('PORT', 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Parking App')
+    .setDescription('The Parking App API')
+    .setVersion('0.1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT);
 
